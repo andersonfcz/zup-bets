fc39a2e7fc5c92ad6d1775cb46d7ed54fc305084
Anderson Pereira <61950773+andersonfcz@users.noreply.github.com>
2021-01-26 13:42:27 -0300

    Initial commit

---
+++ src/main/java/zup/orangetalents/zupbets/ZupBetsApplication.java
@@ -0,0 +1,13 @@
+package zup.orangetalents.zupbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupBetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupBetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/orangetalents/zupbets/ZupBetsApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.orangetalents.zupbets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupBetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
